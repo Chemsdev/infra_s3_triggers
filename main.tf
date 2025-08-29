82d5091f729f19d6d2229d0cb3b088c63fdef315
Chemsdev <chemscreative@gmail.com>
2025-08-29 03:00:25 +0200

    deploiement

--- main.tf
+++ main.tf
@@ -3,19 +3,19 @@ provider "aws" {
 }
 
 # === Résolution des ARN des Lambdas par nom ===
-data "aws_lambda_function" "hubspot-get-pdf-of-day" {
+data "aws_lambda_function" "hubspot_get_pdf_of_day" {
   function_name = "hubspot-get-pdf-of-day"
 }
 
-data "aws_lambda_function" "hubspot-pdf-ocr-processor" {
+data "aws_lambda_function" "hubspot_pdf_ocr_processor" {
   function_name = "hubspot-pdf-ocr-processor"
 }
 
-data "aws_lambda_function" "hubspot-llm-generate-json" {
+data "aws_lambda_function" "hubspot_llm_generate_json" {
   function_name = "hubspot-llm-generate-json"
 }
 
-data "aws_lambda_function" "hubspot-create-deal" {
+data "aws_lambda_function" "hubspot_create_deal" {
   function_name = "hubspot-create-deal"
 }
 
@@ -23,7 +23,7 @@ data "aws_lambda_function" "hubspot-create-deal" {
 resource "aws_lambda_permission" "allow_s3_pdf_ocr" {
   statement_id  = "AllowExecutionFromS3BucketPDF"
   action        = "lambda:InvokeFunction"
-  function_name = data.aws_lambda_function.hubspot-pdf-ocr-processor.function_name
+  function_name = data.aws_lambda_function.hubspot_pdf_ocr_processor.function_name
   principal     = "s3.amazonaws.com"
   source_arn    = "arn:aws:s3:::hubspot-tickets-pdf"
 }
@@ -31,7 +31,7 @@ resource "aws_lambda_permission" "allow_s3_pdf_ocr" {
 resource "aws_lambda_permission" "allow_s3_llm_generate" {
   statement_id  = "AllowExecutionFromS3BucketOCR"
   action        = "lambda:InvokeFunction"
-  function_name = data.aws_lambda_function.hubspot-llm-generate-json.function_name
+  function_name = data.aws_lambda_function.hubspot_llm_generate_json.function_name
   principal     = "s3.amazonaws.com"
   source_arn    = "arn:aws:s3:::hubspot-tickets-pdf"
 }
@@ -39,45 +39,44 @@ resource "aws_lambda_permission" "allow_s3_llm_generate" {
 resource "aws_lambda_permission" "allow_s3_create_deal" {
   statement_id  = "AllowExecutionFromS3BucketDeals"
   action        = "lambda:InvokeFunction"
-  function_name = data.aws_lambda_function.hubspot-create-deal.function_name
+  function_name = data.aws_lambda_function.hubspot_create_deal.function_name
   principal     = "s3.amazonaws.com"
   source_arn    = "arn:aws:s3:::hubspot-tickets-pdf"
 }
 
-# === Permission CloudWatch → Lambda pour cron ===
-resource "aws_lambda_permission" "allow_cloudwatch_get_pdf" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = data.aws_lambda_function.hubspot-get-pdf-of-day.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.get_pdf_daily.arn
-
-  depends_on = [
-    aws_cloudwatch_event_rule.get_pdf_daily
-  ]
-
-  lifecycle {
-    ignore_changes = [
-      function_name,
-      principal,
-      statement_id
-    ]
-  }
-}
-
-# === Déclencheur cron pour hubspot-get-pdf-of-day ===
+# === Cron EventBridge rule pour hubspot-get-pdf-of-day ===
 resource "aws_cloudwatch_event_rule" "get_pdf_daily" {
   name                = "get-pdf-of-day-daily"
   schedule_expression = "cron(0 6 * * ? *)"
 }
 
+# === Permission CloudWatch → Lambda via null_resource pour éviter le blocage ===
+resource "null_resource" "allow_cloudwatch_get_pdf" {
+  provisioner "local-exec" {
+    command = <<EOT
+aws lambda add-permission \
+  --function-name hubspot-get-pdf-of-day \
+  --statement-id AllowExecutionFromCloudWatch \
+  --action lambda:InvokeFunction \
+  --principal events.amazonaws.com \
+  --source-arn ${aws_cloudwatch_event_rule.get_pdf_daily.arn}
+EOT
+  }
+
+  depends_on = [
+    aws_cloudwatch_event_rule.get_pdf_daily,
+    data.aws_lambda_function.hubspot_get_pdf_of_day
+  ]
+}
+
+# === EventBridge target pour hubspot-get-pdf-of-day ===
 resource "aws_cloudwatch_event_target" "target_get_pdf_daily" {
   rule      = aws_cloudwatch_event_rule.get_pdf_daily.name
   target_id = "hubspot-get-pdf-of-day"
-  arn       = data.aws_lambda_function.hubspot-get-pdf-of-day.arn
+  arn       = data.aws_lambda_function.hubspot_get_pdf_of_day.arn
 
   depends_on = [
-    aws_lambda_permission.allow_cloudwatch_get_pdf
+    null_resource.allow_cloudwatch_get_pdf
   ]
 }
 
@@ -86,19 +85,19 @@ resource "aws_s3_bucket_notification" "triggers_hubspot_project" {
   bucket = "hubspot-tickets-pdf"
 
   lambda_function {
-    lambda_function_arn = data.aws_lambda_function.hubspot-pdf-ocr-processor.arn
+    lambda_function_arn = data.aws_lambda_function.hubspot_pdf_ocr_processor.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "PDF_TEST/"
   }
 
   lambda_function {
-    lambda_function_arn = data.aws_lambda_function.hubspot-llm-generate-json.arn
+    lambda_function_arn = data.aws_lambda_function.hubspot_llm_generate_json.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "OCR_PDF/"
   }
 
   lambda_function {
-    lambda_function_arn = data.aws_lambda_function.hubspot-create-deal.arn
+    lambda_function_arn = data.aws_lambda_function.hubspot_create_deal.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "DEAL_JSON/"
   }
@@ -108,10 +107,4 @@ resource "aws_s3_bucket_notification" "triggers_hubspot_project" {
     aws_lambda_permission.allow_s3_llm_generate,
     aws_lambda_permission.allow_s3_create_deal,
   ]
-
-  lifecycle {
-    ignore_changes = [
-      lambda_function
-    ]
-  }
 }
