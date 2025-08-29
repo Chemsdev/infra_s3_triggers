f98abc13329618cf72d1b20c622c7b674c5f65f7
Chemsdev <chemscreative@gmail.com>
2025-08-29 03:15:15 +0200

    deploiement

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "aws_s3_bucket_notification" "triggers_hubspot_project" {
   lambda_function {
     lambda_function_arn = data.aws_lambda_function.hubspot_llm_generate_json.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "OCR_PDF/"
+    filter_prefix       = "PDF_OCR/"
   }
 
   lambda_function {
