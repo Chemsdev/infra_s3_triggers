91c84dfa9b773207d023e4caa390d836a1c95d5f
Chemsdev <chemscreative@gmail.com>
2025-08-29 03:07:08 +0200

    deploiement

--- main.tf
+++ main.tf
@@ -2,11 +2,6 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-# === Résolution des ARN des Lambdas par nom ===
-data "aws_lambda_function" "hubspot_get_pdf_of_day" {
-  function_name = "hubspot-get-pdf-of-day"
-}
-
 data "aws_lambda_function" "hubspot_pdf_ocr_processor" {
   function_name = "hubspot-pdf-ocr-processor"
 }
@@ -44,43 +39,6 @@ resource "aws_lambda_permission" "allow_s3_create_deal" {
   source_arn    = "arn:aws:s3:::hubspot-tickets-pdf"
 }
 
-# === Cron EventBridge rule pour hubspot-get-pdf-of-day ===
-resource "aws_cloudwatch_event_rule" "get_pdf_daily" {
-  name                = "get-pdf-of-day-daily"
-  schedule_expression = "cron(0 6 * * ? *)"
-}
-
-# === Permission CloudWatch → Lambda stable ===
-resource "aws_lambda_permission" "allow_cloudwatch_get_pdf" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = data.aws_lambda_function.hubspot_get_pdf_of_day.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.get_pdf_daily.arn
-
-  depends_on = [
-    aws_cloudwatch_event_rule.get_pdf_daily,
-    data.aws_lambda_function.hubspot_get_pdf_of_day
-  ]
-
-  lifecycle {
-    ignore_changes = [
-      statement_id,
-      source_arn
-    ]
-  }
-}
-
-# === EventBridge target pour hubspot-get-pdf-of-day ===
-resource "aws_cloudwatch_event_target" "target_get_pdf_daily" {
-  rule      = aws_cloudwatch_event_rule.get_pdf_daily.name
-  target_id = "hubspot-get-pdf-of-day"
-  arn       = data.aws_lambda_function.hubspot_get_pdf_of_day.arn
-
-  depends_on = [
-    aws_lambda_permission.allow_cloudwatch_get_pdf
-  ]
-}
 
 # === Notifications S3 pour les autres lambdas ===
 resource "aws_s3_bucket_notification" "triggers_hubspot_project" {
